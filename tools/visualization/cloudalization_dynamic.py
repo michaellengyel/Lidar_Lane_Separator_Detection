79149ea8e58350adb9cec098232ac5ac214cfc10
Michael Lengyel <comptonpark@gmail.com>
2020-07-09 18:51:02 +0200

    Adding point cloud visualization tool; cloudalization

--- tools/visualization/cloudalization_dynamic.py
+++ tools/visualization/cloudalization_dynamic.py
@@ -1,6 +1,8 @@
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 import pandas
+import numpy as np
 
 
 """Planned stratagy for data visualization is as follows. In every cycle the software reads in the next 360 degrees of
@@ -8,42 +10,52 @@ data from the .csv file. This 360 degrees of data will then be displayed as a si
 has a significant duration in time (1/t) during which the vehicle will move forward, this will cause an offset for
 which the visualization must correct. Since no vehicle speed is provided, it is not clear how this will be done."""
 
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+data_skip = 1
+
+
 """Reads in a Frame of data from the .csv file. In each cycle it reads in the next frame of data. Probably also has to
 check what that next data exactly means since it seems the lidar's rotation and it's scans are not in sync."""
 #TODO: Parameterize
 #TODO: Handle readfile exception
-def readData():
-
-    df = pandas.read_csv('../../Lidar_Lane_Separator_Detection/res/1.csv', skiprows=33143, nrows=2803, header=0, usecols=[0, 1, 2])
-    #print(df)
-
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-
-    x = df.iloc[:, 0]
-    y = df.iloc[:, 1]
-    z = df.iloc[:, 2]
-
-    ax.scatter(x, y, z, c='r', marker='.')
+def init_func():
 
     ax.set_xlabel('x')
     ax.set_ylabel('y')
     ax.set_zlabel('z')
 
-    plt.show()
 
 """All rendering of the frame should be done in this function"""
 #TODO: Move rendering from readDate() to this function
-def renderData():
-    print()
+def update_plot(i):
+    #ax.clear()
+    df = pandas.read_csv('../../Lidar_Lane_Separator_Detection/res/4.csv', skiprows=i*400, nrows=64, header=1,
+                         usecols=[0, 1, 2])
+
+    print(i)
+
+    x = df.iloc[:, 0]
+    y = df.iloc[:, 1]
+    z = df.iloc[:, 2]
+
+    ax.set_xlim3d(-10, 10)
+    ax.set_ylim3d(-10, 10)
+    ax.set_zlim3d(-3, 3)
+
+    ax.scatter(x, y, z, c='r', marker='.')
+
 
 """Main function is responsible for the sliding window logic. This logic will feed data to the helper funtions
 which then load the next frame and render it"""
 # TODO: Implement
+
+
 def main():
 
-    readData()
-    renderData()
+    anim = FuncAnimation(fig, update_plot, frames=np.arange(0, 5000, 1), init_func=init_func, interval=30)
+    plt.show()
 
 if __name__ == "__main__":
+
     main()
---
+++ tools/visualization/cloudalization_static.py
@@ -0,0 +1,45 @@
+"""The cloudalization_static.py takes the data of a single revolution and displays it. This is a simplification of the
+cloudalization_dynamic.py, in that it does not update animate the data via a sliding window. In this context, a 'frame'
+means all the data generated during single 360 degrees rotation of the lidar."""
+
+# Imports
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import pandas
+import numpy as np
+
+# Global Variables
+measurement_name = '../../Lidar_Lane_Separator_Detection/res/5.csv'
+
+
+def render_data():
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    data_skip = 1
+
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('z')
+
+    df = pandas.read_csv(measurement_name, usecols=[0, 1, 2])
+
+    x = df.iloc[:, 0]
+    y = df.iloc[:, 1]
+    z = df.iloc[:, 2]
+
+    ax.scatter(x, y, z, c='g', marker='.', s=1)
+
+    ax.set_xlim3d(-15, 15)
+    ax.set_ylim3d(-15, 15)
+    ax.set_zlim3d(-3, 3)
+
+    plt.show()
+
+
+def main():
+    render_data()
+
+
+if __name__ == "__main__":
+    main()
