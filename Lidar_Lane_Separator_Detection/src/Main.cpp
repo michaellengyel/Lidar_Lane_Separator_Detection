9e48534ddc7906fbe4113e62e643bacd42e5b8cc
Michael Lengyel <comptonpark@gmail.com>
2020-07-10 21:51:24 +0200

    Adding initial visualization class frame

--- Lidar_Lane_Separator_Detection/src/Main.cpp
+++ Lidar_Lane_Separator_Detection/src/Main.cpp
@@ -1,12 +1,24 @@
 #include <iostream>
 
 #include "IOHandler.h"
+#include "Scan.h"
+#include "Preprocessor.h"
+#include "Detector.h"
+#include "Visualization.h"
 
 int main() {
 
-	std::cout << "Hello World" << std::endl;
-
+	// Declaring data container and handling classes
 	IOHandler ioHandler("res/2.csv");
+	Scan scan;
+
+	// Declaring data processing classes
+	Preprocessor preprocessor(scan, ioHandler);
+	Detector detector(scan);
+
+	// Declaring data and results visualization classes
+	Visualization visualization(scan);
+
 
 	std::string s = ioHandler.readData(9, 1);
 	std::cout << s << std::endl;
--- Lidar_Lane_Separator_Detection/src/Visualization.h
+++ Lidar_Lane_Separator_Detection/src/Visualization.h
@@ -2,28 +2,24 @@
 // Copyright Disclaimer Placeholder
 //============================================================================================================
 
-#ifndef PREPROCESSOR
-#define PREPROCESSOR
+#ifndef VISUALIZATON
+#define VISUALIZATON
 
-#include "IOHandler.h"
 #include "Scan.h"
 
 //============================================================================================================
-// The Preprocessor is a precuresor to the algorithm. It understands the raw data coming from the sensor and
-// filteres out the unneeded data. This may include fixing missing data points by used the 2nd return,
-// filtering out the 2nd return, filtering out the points which are out of scope etc.
-// The Preprocessor's job is filling the Scan data structure with the minimum valid data needed by the algo.
+// Class Documentation Placeholder
 // TODO: Switch to Doxygen format(///)
 //============================================================================================================
 
-class Preprocessor {
+class Visualization {
 public:
-	
-	Preprocessor(Scan& scan, IOHandler& ioHandler) {
+
+	Visualization(const Scan& scan) {
 
 	}
 
-	Preprocessor() {
+	~Visualization() {
 
 	}
 
