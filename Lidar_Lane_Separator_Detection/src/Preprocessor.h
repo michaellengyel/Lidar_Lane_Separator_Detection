f4f8e05e6310e33705964d6d3ccff3136cc277e6
Michael Lengyel <comptonpark@gmail.com>
2020-07-10 20:38:24 +0200

    Adding data preprocessor class frame

---
+++ Lidar_Lane_Separator_Detection/src/Preprocessor.h
@@ -0,0 +1,34 @@
+//============================================================================================================
+// Copyright Disclaimer Placeholder
+//============================================================================================================
+
+#ifndef PREPROCESSOR
+#define PREPROCESSOR
+
+#include "IOHandler.h"
+#include "Scan.h"
+
+//============================================================================================================
+// The Preprocessor is a precuresor to the algorithm. It understands the raw data coming from the sensor and
+// filteres out the unneeded data. This may include fixing missing data points by used the 2nd return,
+// filtering out the 2nd return, filtering out the points which are out of scope etc.
+// The Preprocessor's job is filling the Scan data structure with the minimum valid data needed by the algo.
+// TODO: Switch to Doxygen format(///)
+//============================================================================================================
+
+class Preprocessor {
+public:
+	
+	Preprocessor(const Scan& scan, const IOHandler& ioHandler) {
+
+	}
+
+	Preprocessor() {
+
+	}
+
+private:
+
+};
+
+#endif
