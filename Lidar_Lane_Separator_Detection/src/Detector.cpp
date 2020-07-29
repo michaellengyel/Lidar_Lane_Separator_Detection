e091a2f5b9340f081000094cbd26c0550cb75e7f
Michael Lengyel <comptonpark@gmail.com>
2020-07-30 00:02:19 +0200

    Implementing initial prototype of algorithm

--- Lidar_Lane_Separator_Detection/src/Detector.cpp
+++ Lidar_Lane_Separator_Detection/src/Detector.cpp
@@ -1,7 +1,32 @@
 #include "Detector.h"
 
 void Detector::algorithm() {
-	std::cout << m_scan.m_data.size() << std::endl;
+
+	// Cluster the data into space boxes
+	cluster();
+
+	// Sort all datapoints in all slices based on size of x component
+	std::sort(m_xFrontRight.begin(), m_xFrontRight.end(), std::greater<double>());
+	std::sort(m_xRearRight.begin(), m_xRearRight.end(), std::greater<double>());
+
+	std::sort(m_xFrontLeft.begin(), m_xFrontLeft.end(), std::greater<double>());
+	std::sort(m_xRearLeft.begin(), m_xRearLeft.end(), std::greater<double>());
+
+	// Test print
+	/*
+	std::cout.precision(17);
+	
+	for (int i = 100; i < 1100; i++) {
+		//std::cout << (m_xFrontRight.at(i) -m_xFrontRight.at(i + 1)) << std::endl;
+		std::cout << m_xFrontLeft.at(i) << std::endl;
+	}
+	*/
+
+	// Sliding window functions
+	slidingWindow_1();
+	slidingWindow_2();
+	
+	std::cout << "Number of pulse points: " << m_scan.m_data.size() << std::endl;
 
 	double all = 0;
 
@@ -12,9 +37,125 @@ void Detector::algorithm() {
 	std::cout << all/ m_scan.m_data.size() << std::endl;
 
 	// Setting up mock data
-	Point point1(-1.7f, -30.0f, -2.3f);
-	Point point2(-1.7f, 30.0f, -2.3f);
+	//Point point1(-1.7f, -30.0f, -2.3f);
+	//Point point2(-1.7f, 30.0f, -2.3f);
+
+	Point point1(m_frontLeft, -30.0f, -SENSOR_Z);
+	Point point2(m_RearLeft, 30.0f, -SENSOR_Z);
+
 	Segment segment(point1, point2);
 	m_segment = segment;
 
+}
+
+void Detector::cluster() {
+
+	double xCurrent = 0;
+	double yCurrent = 0;
+
+	bool isFrontRight = false;
+	bool isRearRight = false;
+
+	bool isFrontLeft = false;
+	bool isRearLeft = false;
+
+	for (int i = 0; i < m_scan.m_data.size(); i++) {
+
+		xCurrent = m_scan.m_data.at(i).m_point.m_xPos;
+		yCurrent = m_scan.m_data.at(i).m_point.m_yPos;
+
+		isFrontRight = (((yCurrent >= FRONT_MIN) && (yCurrent <= FRONT_MAX)) && (xCurrent >= 0));
+		isRearRight = (((yCurrent < REAR_MAX) && (yCurrent >= REAR_MIN)) && (xCurrent >= 0));
+
+		isFrontLeft = (((yCurrent >= FRONT_MIN) && (yCurrent <= FRONT_MAX)) && (xCurrent < 0));
+		isRearLeft = (((yCurrent < REAR_MAX) && (yCurrent >= REAR_MIN)) && (xCurrent < 0));
+
+		if (isFrontRight) {
+			m_xFrontRight.push_back(xCurrent);
+		}
+		else if (isRearRight) {
+			m_xRearRight.push_back(xCurrent);
+		}
+		else if (isFrontLeft) {
+			m_xFrontLeft.push_back(xCurrent);
+		}
+		else if (isRearLeft) {
+			m_xRearLeft.push_back(xCurrent);
+		}
+	}
+
+	std::cout << "m_xFrontRight" << m_xFrontRight.size() << std::endl;
+	std::cout << "m_xRearRight" << m_xRearRight.size() << std::endl;
+	std::cout << "m_xFrontLeft" << m_xFrontLeft.size() << std::endl;
+	std::cout << "m_xRearLeft" << m_xRearLeft.size() << std::endl;
+}
+
+void Detector::slidingWindow_1() {
+
+	// Calculate average of points 
+	double average = 0;
+	double total = 0;
+
+	for (int i = 0; i < m_xFrontLeft.size() - 1; i++) {
+		total += (m_xFrontLeft.at(i) - m_xFrontLeft.at(i + 1));
+	}
+
+	average = total / m_xFrontLeft.size();
+	std::cout << "Average: " << average << std::endl;
+
+	// Sliding window
+	const int WINDOW_SIZE = 1000;
+
+
+	// Pooling ALGO #1
+	for (int i = 0; i < m_xFrontLeft.size() - WINDOW_SIZE; i++) {
+		double windowTotal = 0;
+		for (int j = 0; j < WINDOW_SIZE - 1; j++) {
+			windowTotal += (m_xFrontLeft.at(i + j) - m_xFrontLeft.at(i + j + 1));
+		}
+		double windowAverage = windowTotal / WINDOW_SIZE;
+		//std::cout << "windowAverage: " << windowAverage << std::endl;
+		if ((windowAverage * 4) < average) {
+			std::cout << "PING" << std::endl;
+			std::cout << m_xFrontLeft.at(i) << std::endl;
+			m_frontLeft = m_xFrontLeft.at(i);
+			return;
+		}
+
+	}
+}
+
+void Detector::slidingWindow_2() {
+
+	// Calculate average of points 
+	double average = 0;
+	double total = 0;
+
+	for (int i = 0; i < m_xRearLeft.size() - 1; i++) {
+		total += (m_xRearLeft.at(i) - m_xRearLeft.at(i + 1));
+	}
+
+	average = total / m_xRearLeft.size();
+	std::cout << "Average: " << average << std::endl;
+
+	// Sliding window
+	const int WINDOW_SIZE = 1000;
+
+
+	// Pooling ALGO #1
+	for (int i = 0; i < m_xRearLeft.size() - WINDOW_SIZE; i++) {
+		double windowTotal = 0;
+		for (int j = 0; j < WINDOW_SIZE - 1; j++) {
+			windowTotal += (m_xRearLeft.at(i + j) - m_xRearLeft.at(i + j + 1));
+		}
+		double windowAverage = windowTotal / WINDOW_SIZE;
+		//std::cout << "windowAverage: " << windowAverage << std::endl;
+		if ((windowAverage * 4) < average) {
+			std::cout << "PING" << std::endl;
+			std::cout << m_xRearLeft.at(i) << std::endl;
+			m_RearLeft = m_xFrontLeft.at(i);
+			return;
+		}
+
+	}
 }
--- Lidar_Lane_Separator_Detection/src/Detector.h
+++ Lidar_Lane_Separator_Detection/src/Detector.h
@@ -8,6 +8,10 @@
 #include "Scan.h"
 #include "Segment.h"
 
+#include <vector>
+#include <algorithm>
+#include <functional>
+
 //============================================================================================================
 // Class Documentation Placeholder
 // TODO: Switch to Doxygen format(///)
@@ -30,7 +34,33 @@ public:
 
 private:
 
+	void cluster();
+
+	void slidingWindow_1();
+	void slidingWindow_2();
+	double m_frontLeft = 0;
+	double m_RearLeft = 0;
+
 	const Scan& m_scan;
+
+	// Holds y-axis point data of frontal section
+	std::vector<double> m_xFrontRight;
+	// Holds y-axis point data of rear section
+	std::vector<double> m_xRearRight;
+	// Holds y-axis point data of frontal section
+	std::vector<double> m_xFrontLeft;
+	// Holds y-axis point data of rear section
+	std::vector<double> m_xRearLeft;
+
+	// Config parameters
+
+	const double FRONT_MIN = 0;
+	const double FRONT_MAX = 30;
+
+	const double REAR_MAX = 0;
+	const double REAR_MIN = -30;
+
+	const double SENSOR_Z = 2.3;
 
 };
 
