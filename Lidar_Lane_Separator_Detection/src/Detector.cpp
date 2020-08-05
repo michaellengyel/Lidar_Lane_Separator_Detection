815b87c9641f6aee06e0bb0e4a5783f239a427d7
Michael Lengyel <comptonpark@gmail.com>
2020-08-05 02:31:17 +0200

    Implementing initial gridMaker function

--- Lidar_Lane_Separator_Detection/src/Detector.cpp
+++ Lidar_Lane_Separator_Detection/src/Detector.cpp
@@ -2,6 +2,11 @@
 
 void Detector::algorithm() {
 
+	// Iterate through all points in the point cloud
+	for (int i = 0; i < m_scan.m_data.size(); i++) {
+
+	}
+
 	std::cout << "Total number of pulses: " << m_scan.m_data.size() << std::endl;
 
 	double all = 0;
@@ -33,5 +38,39 @@ void Detector::algorithm() {
 	m_segments.push_back(segment2);
 	m_segments.push_back(segment3);
 	m_segments.push_back(segment4);
+
+}
+
+void Detector::gridMaker() {
+
+	double yIncrement = DELTA_Y / NUMBER_OF_BOXES;
+	double xIncrement = DELTA_X / NUMBER_OF_BOXES;
+
+	double upperYLimit = MIN_Y + yIncrement;
+	double lowerYLimit = MIN_Y;
+	double upperXLimit = MIN_X + xIncrement;
+	double lowerXLimit = MIN_X;
+
+	for (int i = 0; i < NUMBER_OF_BOXES; i++) {
+		for (int j = 0; j < NUMBER_OF_BOXES; j++) {
+			GridBox gridBox(upperYLimit, lowerYLimit, upperXLimit, lowerXLimit);
+			m_boxes.push_back(gridBox);
+
+			upperXLimit += xIncrement;
+			lowerXLimit += xIncrement;
+		}
+
+		upperXLimit = MIN_X + xIncrement;
+		lowerXLimit = MIN_X;
+
+		upperYLimit += yIncrement;
+		lowerYLimit += yIncrement;
+	}
+
+	for (int i = 0; i < m_boxes.size(); i++) {
+		//std::cout << m_boxes.at(i).m_upperYLimit << "\t" << m_boxes.at(i).m_lowerYLimit << "\t" << m_boxes.at(i).m_upperXLimit << "\t" << m_boxes.at(i).m_lowerXLimit << std::endl;
+	}
+
+	std::cout << "Number of boxes (pixels): " << m_boxes.size() << std::endl;
 
 }
--- Lidar_Lane_Separator_Detection/src/Detector.h
+++ Lidar_Lane_Separator_Detection/src/Detector.h
@@ -5,8 +5,12 @@
 #ifndef DETECTOR
 #define DETECTOR
 
+#include <math.h>
+#include <vector>
+
 #include "Scan.h"
 #include "Segment.h"
+#include "Enumerations.h"
 
 //============================================================================================================
 // Class Documentation Placeholder
@@ -17,6 +21,8 @@ class Detector {
 public:
 
 	Detector(const Scan& scan) : m_scan(scan) {
+
+		gridMaker();
 
 	}
 
@@ -31,8 +37,60 @@ public:
 
 private:
 
+	// Hough Space
+	class Accumulator {
+	public:
+
+		static unsigned const int RO_RESOLUTION = 100; // Radius
+		static unsigned const int THETA_RESOLUTION = 100; // Angle
+		unsigned int m_accumulatorTable[RO_RESOLUTION][THETA_RESOLUTION];
+
+	};
+
+	// Space box container
+	class GridBox {
+	public:
+
+		GridBox(double upperYLimit, double lowerYLimit, double upperXLimit, double lowerXLimit) :
+			m_upperYLimit(upperYLimit),
+			m_lowerYLimit(lowerYLimit),
+			m_upperXLimit(upperXLimit),
+			m_lowerXLimit(lowerXLimit)
+		{
+
+		}
+
+		unsigned const int m_weight = 0;
+
+		const double m_upperYLimit;
+		const double m_lowerYLimit;
+		const double m_upperXLimit;
+		const double m_lowerXLimit;
+	};
+
+	// Member container reference for Pulse data
 	const Scan& m_scan;
 
-};
+	// Member container for grid boxes
+	std::vector<GridBox> m_boxes;
 
+	// The grid maker divides the x-y plane into a grid of a parameterizable resolution
+	// where the value of a given box is determined by the number of reflections contained
+	// within it's borders.
+	void gridMaker();
+
+	// Config parameters
+
+	const double MAX_Y = static_cast<double>(algoParameters::MAX_Y);
+	const double MIN_Y = static_cast<double>(algoParameters::MIN_Y);
+
+	const double MAX_X = static_cast<double>(algoParameters::MAX_X);
+	const double MIN_X = static_cast<double>(algoParameters::MIN_X);
+
+	const double DELTA_Y = MAX_Y - MIN_Y;
+	const double DELTA_X = MAX_X - MIN_X;
+
+	const int NUMBER_OF_BOXES = 100; // Number of containers along the x and y axis
+
+};
 #endif
